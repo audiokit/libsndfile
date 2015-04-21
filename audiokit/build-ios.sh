950c1004d97b078f40cbc715146fc79232d6c555
Stephane Peter <megastep@megastep.org>
2015-04-21 14:49:35 -0700

    Added static library target in CMake, working building script for iOS for both dylib and static.

--- audiokit/build-ios.sh
+++ audiokit/build-ios.sh
@@ -6,33 +6,31 @@ if test $# -lt 1; then
 	exit 1
 fi
 
-# Get ready
-cd ..
-./autogen.sh
-./autogen.sh cmake
 
 BUILDCONF=${BUILDCONF:-Release}
 if test "$1" == "static";
 then
 	EXT=a
-	STYLE=staticlib
-	./configure --disable-shared
+	TARGET=sndfile_static
 else
 	EXT=dylib
-	STYLE=mh_dylib
-	./configure --enable-shared
+	TARGET=sndfile
 fi
 
-FLAGS="MACH_O_TYPE=$STYLE"
+echo "Building libsndfile.$EXT for iOS, config $BUILDCONF ..."
 
-echo "Building libsndfile.$EXT for iOS, config $BUILDCONF, style $STYLE ..."
+# Get ready
+cd ..
+./autogen.sh
+./autogen.sh cmake
+./configure
 
 cmake . -G Xcode -DCMAKE_BUILD_TYPE=$BUILDCONF || exit 1
 
-(xcodebuild -sdk iphoneos -xcconfig audiokit/device.xcconfig -target sndfile -configuration $BUILDCONF $FLAGS | xcpretty -c) || exit 1
-cp src/$BUILDCONF/libsndfile.1.$EXT audiokit/libsndfile-dev.$EXT
-(xcodebuild -sdk iphonesimulator -xcconfig audiokit/simulator.xcconfig -target sndfile -configuration $BUILDCONF $FLAGS | xcpretty -c) || exit 1
-cp src/$BUILDCONF/libsndfile.1.$EXT audiokit/libsndfile-sim.$EXT
+(xcodebuild -sdk iphoneos -xcconfig audiokit/device.xcconfig -target $TARGET -configuration $BUILDCONF | xcpretty -c) || exit 1
+cp src/$BUILDCONF/lib$TARGET.$EXT audiokit/libsndfile-dev.$EXT
+(xcodebuild -sdk iphonesimulator -xcconfig audiokit/simulator.xcconfig -target $TARGET -configuration $BUILDCONF | xcpretty -c) || exit 1
+cp src/$BUILDCONF/lib$TARGET.$EXT audiokit/libsndfile-sim.$EXT
 
 # Combine architectures into the final library
 lipo -create audiokit/libsndfile-dev.$EXT audiokit/libsndfile-sim.$EXT -output audiokit/libsndfile.1.$EXT
