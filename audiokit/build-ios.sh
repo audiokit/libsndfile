8f299aba0a0a790962b7ef751d552a9be47616b3
Stephane Peter <megastep@megastep.org>
2015-04-22 01:27:33 -0700

    Fully functional build scripts to build static and dynamic libraries for AudioKit.

--- audiokit/build-ios.sh
+++ audiokit/build-ios.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-# Build a universal dylib version of libsndfile for iOS
+#
+# Build a universal version of libsndfile for iOS suitable to use within AudioKit projects.
+#
+# (C) 2015 Stephane Peter 
+#
 
 if test $# -lt 1; then
 	echo "Usage: $0 dynamic|static"
 	exit 1
 fi
 
+# Default: AudioKit in the same parent directory as libsndfile
+AK_ROOT=${AK_ROOT:-$PWD/../../AudioKit/AudioKit/Platforms/iOS}
 
 BUILDCONF=${BUILDCONF:-Release}
 if test "$1" == "static";
 then
-	EXT=a
+	EXT=.a
 	TARGET=sndfile_static
 else
-	EXT=dylib
+	EXT=.dylib
 	TARGET=sndfile
 fi
 
-echo "Building libsndfile.$EXT for iOS, config $BUILDCONF ..."
+echo "Building libsndfile$EXT for iOS, config $BUILDCONF ..."
 
 # Get ready
 cd ..
@@ -28,10 +34,21 @@ cd ..
 cmake . -G Xcode -DCMAKE_BUILD_TYPE=$BUILDCONF || exit 1
 
 (xcodebuild -sdk iphoneos -xcconfig audiokit/device.xcconfig -target $TARGET -configuration $BUILDCONF | xcpretty -c) || exit 1
-cp src/$BUILDCONF/lib$TARGET.$EXT audiokit/libsndfile-dev.$EXT
+cp src/$BUILDCONF/lib$TARGET$EXT audiokit/libsndfile-dev$EXT
 (xcodebuild -sdk iphonesimulator -xcconfig audiokit/simulator.xcconfig -target $TARGET -configuration $BUILDCONF | xcpretty -c) || exit 1
-cp src/$BUILDCONF/lib$TARGET.$EXT audiokit/libsndfile-sim.$EXT
+cp src/$BUILDCONF/lib$TARGET$EXT audiokit/libsndfile-sim$EXT
 
 # Combine architectures into the final library
-lipo -create audiokit/libsndfile-dev.$EXT audiokit/libsndfile-sim.$EXT -output audiokit/libsndfile.1.$EXT
+lipo -create audiokit/libsndfile-dev$EXT audiokit/libsndfile-sim$EXT -output audiokit/libsndfile$EXT
+
+# Copy the file to the AudioKit image
+if test "$1" == "static";
+then
+	cp -v audiokit/libsndfile$EXT $AK_ROOT/libs/
+else
+	install_name_tool -id libsndfile audiokit/libsndfile$EXT
+        cp -v audiokit/libsndfile$EXT $AK_ROOT/libsndfile.framework/libsndfile
+fi
+
+echo "Success!"
 
